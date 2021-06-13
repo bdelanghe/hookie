e45ba9ce7879bdbf44f9be0ab3dec6d78a470f3b
Robert DeLanghe <bdelanghe@gmail.com>
2021-06-13 18:38:48 -0400

    changed to literals

--- src/words.js
+++ src/words.js
@@ -12,16 +12,16 @@ const counter = (bag) => {
 export const wordsList = (bag) => {
     let words = [];
     const counts = counter(bag);
-    let to_check = [{"node": root, "countsIndex": 0}];
+    let to_check = [{node: root, countsIndex: 0}];
     while (to_check.length > 0) {
         const {node, countsIndex} = to_check.pop();
         if ('words' in node) {
-            words.push(...node['words'])
+            words.push(...node.words)
         } else {
             const count = counts[countsIndex];
             for (let n = 0; n <= count; n++) {
                 if (n in node) {
-                    to_check.push({"node": node[n], "countsIndex": countsIndex + 1})
+                    to_check.push({node: node[n], countsIndex: countsIndex + 1})
                 }
             }
         }
